85d8e77ecc25a90378155c891d2ebb55f8f62324
Swadhin Biswas <107450069+swadhinbiswas@users.noreply.github.com>
2022-09-12 10:14:05 +0600

    Create star print in like square or retangel.c
    Signed-off-by: Swadhin Biswas <107450069+swadhinbiswas@users.noreply.github.com>

---
+++ star print in like square or retangel.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+ int a,b,c;
+ scanf("%d",&a);
+ for(a=0;a<c;a++)
+ {
+    for(b=0;b<=c;b++)
+    { 
+        printf("*");
+    }
+ printf("\n");
+ }
+
+ return 0;
+
+
+
+}
